e5b43364f9f171e692649a5910b6e036e7a2dee2
Julien Look <julien.look@cinoware.com>
2024-10-02 10:17:24 +0200

    .

--- Woche 3/Warmups/Tag3.py
+++ Woche 3/Warmups/Tag3.py
@@ -7,3 +7,24 @@ Beispiel:
 [1, 2, 3, 4] = 2.5
 
 """
+
+
+def median(arr):
+    for i in arr:
+        if type(i) != int and type(i) != float:
+            raise ValueError("Array contains non-numeric values")
+
+    arr.sort()
+    n = len(arr)
+    if n % 2 == 0:
+        upper_bound = n // 2
+        lower_bound = upper_bound - 1
+        arr_upper = arr[upper_bound]
+        arr_lower = arr[lower_bound]
+        return (arr_upper + arr_lower) / 2
+    else:
+        return arr[n // 2]
+
+
+print(median([23, 34, 15]) == 23)
+print(median([1, 2, 3, 4]) == 2.5)
